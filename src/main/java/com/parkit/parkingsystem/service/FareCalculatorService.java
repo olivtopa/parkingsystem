9dc3d811a7ec4130cff4a2e919aeff8981d3e69a
Olivier <amoliv_825@hotmail.com>
2021-06-21 12:29:55 +0200

    Ajout du paramètre boolean et du calcul de la réduction de 5 pourcents à la classe FareCalculatorService

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,31 +5,37 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
-        @Deprecated
-        double inHour = ticket.getInTime().getTime(); 
-        double outHour = ticket.getOutTime().getTime();
-              
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        
-        double duration = (outHour - inHour)/3600000;
-        
-        if(duration>(0.5)) {
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            	}
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            	}
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        	}
-        }else ticket.setPrice(0.0);
-        
-       }
+	public void calculateFare(Ticket ticket, boolean discount) {
+		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
+		}
+		@Deprecated
+		double inHour = ticket.getInTime().getTime();
+		double outHour = ticket.getOutTime().getTime();
+
+		// TODO: Some tests are failing here. Need to check if this logic is correct
+
+		double duration = (outHour - inHour) / 3600000;
+
+		if (duration > (0.5)) {
+			switch (ticket.getParkingSpot().getParkingType()) {
+			case CAR: {
+				ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+				break;
+			}
+			case BIKE: {
+				ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unkown Parking Type");
+			}
+		} else
+			ticket.setPrice(0.0);
+
+		if (discount) {
+			ticket.setPrice(0.95 * (ticket.getPrice()));
+		}
+
+	}
 }
